40fb9e47c88e96d5a5cccd4c9a535d0ee1dfd877
ehgus0526 <ehgus0526@naver.com>
2025-01-15 01:00:47 +0900

    Setting: 프로젝트 초기 세팅

---
+++ src/app/App.tsx
@@ -0,0 +1,29 @@
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import MainLayOut from '../widgets/MainLayOut';
+import MainPage from '../pages/MainPage';
+
+function App() {
+  return (
+    <Routes>
+      {/* 메인 레이아웃 적용 */}
+      <Route element={<MainLayOut/>}>
+        
+        {/* 메인 페이지 */}
+        <Route path="/" element={<MainPage/>} />
+        
+        {/* 모집 페이지 */}
+        <Route path="/recruitment" element={<MainPage/>} />
+
+        {/* FAQ 페이지 */}
+        <Route path="/faq" element={<MainPage/>} />
+
+        {/* 컨택트 페이지 */}
+        <Route path="/contact" element={<MainPage/>} />
+
+      </Route>
+    </Routes>
+  )
+}
+
+export default App;
---
+++ src/app/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import ScrollToTop from '../shared/utils/scrollToTopFunc.ts';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <RecoilRoot>
+        <ScrollToTop/>
+        <App />
+      </RecoilRoot>
+    </BrowserRouter>
+  </StrictMode>
+)
