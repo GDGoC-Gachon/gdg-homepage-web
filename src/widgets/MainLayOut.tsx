d576bf26471387b147215efa8b4cff259b9f1113
odukong <ohbin1017@gmail.com>
2025-01-15 20:16:28 +0900

    style : vsc tab, space size 변경으로 인한 코드 포맷 변경
    ref : #1

--- src/widgets/MainLayOut.tsx
+++ src/widgets/MainLayOut.tsx
@@ -3,15 +3,15 @@ import Header from "../shared/components/Header";
 import Footer from "../shared/components/Footer";
 
 function MainLayOut() {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <Header />
-            <div className="flex-grow pt-[4.125rem]">
-                <Outlet />
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow pt-[4.125rem]">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default MainLayOut;
