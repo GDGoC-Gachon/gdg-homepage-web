320722acd08757a377c23f57ac513330d52fe4cf
ehgus0526 <ehgus0526@naver.com>
2025-01-26 22:53:32 +0900

    feat : 홈화면 디테일 기능 구현 ref : #9

--- src/features/main/ui/WelcomeSection.tsx
+++ src/features/main/ui/WelcomeSection.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as ArrowIcon } from '../../../shared/assets/icons/main/arrow.svg';
-import { RecruitButton } from './RecruitButotn';
+import { RecruitButton } from './RecruitButton';
+import { Fade } from 'react-awesome-reveal';
 
 type WelcomeSectionProps = {
   onArrowClick: () => void;
@@ -8,8 +9,12 @@ type WelcomeSectionProps = {
 function WelcomeSection({ onArrowClick }: WelcomeSectionProps) {
   return (
     <div className="flex-center flex-col w-screen h-main-section bg-mainDot bg-no-repeat bg-[length:23rem_auto] text-center">
-      <h1 className="text-6xl font-productSans font-bold">Welcome to</h1>
-      <h2 className="mt-6 font-productSans text-[2.8488rem] font-bold">GDG on Campus Gachon</h2>
+      <h1 className="text-6xl font-productSans font-bold">
+        <Fade cascade damping={0.05} triggerOnce>Welcome to</Fade>
+      </h1>
+      <h2 className="mt-6 font-productSans text-[2.8488rem] font-bold">
+        <Fade cascade damping={0.05} delay={500} triggerOnce>GDG on Campus Gachon</Fade>
+      </h2>
       <ArrowIcon
         className="my-10 cursor-pointer animate-fadeInOut"
         onClick={onArrowClick}
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useEffect } from "react";
 import { scrollToSection } from "../features/main/lib/scrollToSection";
 import WelcomeSection from "../features/main/ui/WelcomeSection";
 import DescriptionSection from "../features/main/ui/DescriptionSection";
@@ -7,6 +7,13 @@ import NewsSection from "../features/main/ui/NewsSection";
 function MainPage() {
   const descriptionRef = useRef<HTMLDivElement | null>(null);
   const newsRef = useRef<HTMLDivElement | null>(null);
+  
+  // 새로고침 시 페이지 상단으로 이동
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 0);
+  }, []);
   
   return (
     <div 
