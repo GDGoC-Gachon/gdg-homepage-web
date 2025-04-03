9b422426eb69f8772bf1760f5fb03f191dd6e062
ehgus0526 <ehgus0526@naver.com>
2025-04-03 17:13:43 +0900

    feat : 회원가입 2페이지 에러 핸들링 구현 ref : #21

--- src/features/auth/model/authSchema.ts
+++ src/features/auth/model/authSchema.ts
@@ -1,5 +1,6 @@
 import * as yup from "yup";
 
+// 회원가입 1페이지 스키마
 export const signupStep1Schema = yup.object({
   email: yup
     .string()
@@ -18,3 +19,28 @@ export const signupStep1Schema = yup.object({
     .oneOf([yup.ref("password")], "비밀번호가 일치하지 않습니다.")
     .required("비밀번호 확인이 필요합니다."),
 });
+
+// 회원가입 2페이지 스키마
+export const signupStep2Schema = yup.object({
+  name: yup
+    .string()
+    .matches(/^[가-힣a-zA-Z]{2,}$/, "이름 형식이 올바르지 않습니다.")
+    .required("이름을 입력해주세요."),
+  phoneNumber: yup
+    .string()
+    .required("휴대폰 번호를 입력해주세요.")
+    .min(13, "휴대폰 번호 형식이 올바르지 않습니다."),
+  grade: yup
+    .string()
+    .required("학년을 입력해주세요."),
+  number: yup
+    .string()
+    .required("학번을 입력해주세요.")
+    .matches(/^\d{9}$/, "학번 형식이 올바르지 않습니다."),
+  major: yup
+    .string()
+    .required("전공을 입력해주세요."),
+  info: yup.string().optional().default(""),
+});
+
+export type SignupStep2FormData = yup.InferType<typeof signupStep2Schema>;
